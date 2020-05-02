37a6e1c375fe67ee8fcd8fcb41dc6b4d23b95a37
Darius Costolas <dariuscostolas@yahoo.com>
2020-05-02 18:48:27 +0300

    improve full page spinner

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -3,5 +3,9 @@ import React from 'react';
 import styles from './Spinner.module.scss';
 
 export const Spinner = (): JSX.Element => {
-    return <div className={styles.spinner} />;
+    return (
+        <div className={styles.spinnerWrapper}>
+            <div className={styles.spinner} />
+        </div>
+    );
 }; 
