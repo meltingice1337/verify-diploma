3e729110065c6cab685d637532e3c66c06350861
Darius Costolas <dariuscostolas@yahoo.com>
2020-05-05 00:01:51 +0300

    add image url option besides file upload

--- src/pages/create-certificate/CreateCertificate.tsx
+++ src/pages/create-certificate/CreateCertificate.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
@@ -10,33 +10,41 @@ interface CertificateFormData {
     certificateDescription: string;
     recipientName: string;
     recipientEmail: string;
-    certificateImage: FileList | null;
 }
 
 const CreateCertificate = (): JSX.Element => {
     const { register, handleSubmit, errors, watch, setValue } = useForm<CertificateFormData>();
-    const [certImageData, setCertImageData] = useState<string>();
-
-    const certImage = watch('certificateImage');
+    const [certImageFile, setCertImageFile] = useState<File>();
+    const [certImageUrl, setCertImageUrl] = useState<string>('');
+    const [certImageFileData, setCertImageFileData] = useState<string>();
 
     const onSubmit = handleSubmit((data): void => {
         console.log({ errors });
         console.log({ data });
     });
 
-    useEffect(() => {
-        if (certImage && certImage?.length > 0) {
-            if (!(/\.(gif|jpe?g|tiff|png|webp|bmp)$/i).test(certImage[0].name)) {
-                setValue('certificateImage', null);
+    const onImageFileUploadChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        if (event.target.files && event.target.files?.length > 0) {
+            const imageFile = event.target.files[0];
+            if (!(/\.(gif|jpe?g|tiff|png|webp|bmp)$/i).test(imageFile.name)) {
                 toast.error('The selected file is not an image.');
                 return;
             }
 
+            setCertImageFile(imageFile);
+            setCertImageUrl('');
+
             const reader = new FileReader();
-            reader.readAsDataURL(certImage[0]);
-            reader.onload = (): void => setCertImageData(reader.result as string);
+            reader.readAsDataURL(imageFile);
+            reader.onload = (): void => setCertImageFileData(reader.result as string);
         }
-    }, [certImage]);
+    };
+
+    const onImageUrlChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setCertImageFile(undefined);
+        setCertImageFileData(undefined);
+        setCertImageUrl(event.target.value);
+    };
 
     return (
         <PageContainer>
@@ -66,11 +74,13 @@ const CreateCertificate = (): JSX.Element => {
                         <label htmlFor="description" className="col-sm-2 col-form-label">Image</label>
                         <div className="col-sm-10">
                             <div className="custom-file">
-                                <input ref={register} type="file" name="certificateImage" className="custom-file-input" id="certificateImageInput" aria-describedby="certificateImageInputHelp" />
-                                <label className="custom-file-label" htmlFor="certificateImageInput">{certImage && certImage?.length > 0 ? `${certImage?.[0]?.name} loaded` : 'Load a certificate image'}</label>
+                                <input type="file" name="certificateImageUpload" className="custom-file-input" id="certificateImageFile" aria-describedby="certificateImageFileHelp" onChange={onImageFileUploadChange} />
+                                <label className="custom-file-label" htmlFor="certificateImageInput">{certImageFile ? certImageFile.name : 'Load a certificate image'}</label>
                             </div>
                             <small id="emailHelp" className="form-text text-muted">The image will be base64 encoded in the certificate</small>
-                            {certImageData && <img src={certImageData} className="mb-4" style={{ height: 'auto', width: 'auto', maxWidth: '30rem', maxHeight: '30rem', }} />}
+                            {certImageFileData && <img src={certImageFileData} className="mb-4" style={{ height: 'auto', width: 'auto', maxWidth: '30rem', maxHeight: '30rem', }} />}
+                            <p className="mt-3 mb-3">or</p>
+                            <input name="certificateImageUrl" value={certImageUrl} onChange={onImageUrlChange} type="text" className="form-control" id="recipientName" placeholder="Enter image url ..." />
                         </div>
                     </div>
                     <h4 className="mb-4">Certificate recipient</h4>
